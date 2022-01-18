1c52a8d13210ccaee3d6365507b5e7b6d5233d4a
sungchian <bnbn870829@gmail.com>
2022-01-18 17:29:12 +0800

    add update products info but not render after update

--- YungChing/Controllers/ProductsController.cs
+++ YungChing/Controllers/ProductsController.cs
@@ -101,7 +101,82 @@ namespace YungChing.Controllers
             }
         }
 
+        //編輯功能
+        public async Task<bool> UpdateProd(int Id, string name, string supplierId, string categoryId, string quantityPerUnit, string unitPrice, string unitsInStock, string unitsOnOrder)
+        {
+            try
+            {
+                string connectionString = getAppSetting.getDataBseStr();
+                
+                var sqlStr = "UPDATE [Northwind].[dbo].[Products]  SET [ProductName] = @name, [SupplierID] = @supplierId, [CategoryID] = @categoryId, [QuantityPerUnit] = @quantityPerUnit, [UnitPrice] = @unitPrice, [UnitsInStock] = @unitsInStock, [UnitsOnOrder] = @unitsOnOrder  WHERE [ProductID] = @Id ";
+                using (SqlConnection connect = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand(sqlStr, connect))
+                    {
+                        connect.Open();
+
+                        cmd.Parameters.Add("@Id", SqlDbType.Int).Value = Id;
+                        cmd.Parameters.Add("@name", SqlDbType.NVarChar).Value = string.IsNullOrEmpty(name) ? DBNull.Value : (object)name;
+                        cmd.Parameters.Add("@supplierId", SqlDbType.Int).Value = string.IsNullOrEmpty(supplierId) ? DBNull.Value : (object)supplierId;
+                        cmd.Parameters.Add("@categoryId", SqlDbType.Int).Value = string.IsNullOrEmpty(categoryId) ? DBNull.Value : (object)categoryId;
+                        cmd.Parameters.Add("@quantityPerUnit", SqlDbType.NVarChar).Value = string.IsNullOrEmpty(quantityPerUnit) ? DBNull.Value : (object)quantityPerUnit;
+                        cmd.Parameters.Add("@unitPrice", SqlDbType.Money).Value = string.IsNullOrEmpty(unitPrice) ? DBNull.Value : (object)unitPrice;
+                        cmd.Parameters.Add("@unitsInStock", SqlDbType.SmallInt).Value = string.IsNullOrEmpty(unitsInStock) ? DBNull.Value : (object)unitsInStock;
+                        cmd.Parameters.Add("@unitsOnOrder", SqlDbType.SmallInt).Value = string.IsNullOrEmpty(unitsOnOrder) ? DBNull.Value : (object)unitsOnOrder;
+                        int row = cmd.ExecuteNonQuery();
+                        if (row > 0)
+
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
 
 
+        //刪除功能
+        public async Task<string> DeleteProd(int Id)
+        {
+            try
+            {
+                string connectionString = getAppSetting.getDataBseStr();
+
+                var sqlString = @"DELETE FROM [Northwind].[dbo].[Products] WHERE Id = @id";
+
+                using (SqlConnection connect = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand(sqlString, connect))
+                    {
+                        connect.Open();
+
+                        cmd.Parameters.Add("@id", SqlDbType.Int).Value = Id;
+
+                        int row = cmd.ExecuteNonQuery();
+
+                        if (row > 0)
+                        {
+                            return "success";
+                        }
+                        else
+                        {
+                            return "fail";
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+        }
     }
 }
