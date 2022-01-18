10e912259c00e547fe1e1b9b324a0eee0aa469b1
sungchian <bnbn870829@gmail.com>
2022-01-18 15:00:47 +0800

    add models

---
+++ YungChing/Models/MProducts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YungChing.Models
+{
+    public class MProducts
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int SupplierID { get; set; }
+        public int CategoryID { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public string UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+        public int UnitsOnOrder { get; set; }
+    }
+}
